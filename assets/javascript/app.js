79c24c57bf417d233b3fb94054fb22924c4a191d
Scott Juneau <scott@junlaw.com>
2019-04-02 17:15:38 -0500

    psuedo code in place

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,17 @@
 // Trivia game
 
 //
+var game = {};
+
+// Start game
+
+// Monitor Start button being pressed
+// and call first question
+
+// Load screen and start count down
+// If timer ends and no click, display feedback message
+// On click, determine if right or wrong
+// On click, reset timer
+// Display feedback message
+// Load next screen and start count down
+// End game message
